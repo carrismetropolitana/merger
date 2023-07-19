da146ae8fa45daffdda8094853594089ac7a7936
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-19 20:01:04 +0100

    Set ignore_last_delimiters to true

--- index.js
+++ index.js
@@ -249,7 +249,7 @@ const createOutputFile = (filename, headers) => {
 
 async function importFile(filepath, filename, headers, prefix = '') {
   console.log(`⤷ Importing "${filepath}${filename}" to output file...`);
-  const parserStream = fs.createReadStream(filepath + filename).pipe(parse({ columns: true, trim: true, skip_empty_lines: true, ignore_last_delimiters: true, bom: true }));
+  const parserStream = fs.createReadStream(filepath + filename).pipe(parse({ columns: true, trim: true, skip_empty_lines: true, bom: true }));
   let counter = 0;
   for await (const rowObject of parserStream) {
     counter++;
